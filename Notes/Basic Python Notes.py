60194def85c5f644ecee346b8eb75382446445de
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2017-11-17 15:25:57 -0800

    Initial commit

---
+++ Notes/Basic Python Notes.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print("Welcome to Class!")
+
+# This is a comment!
+# They are surprisingly useful!
+# The program doesn't run these!
+
+'''
+This is some text that will be
+ignored because it is in between
+three quotation marks
+'''
+
+# Variables
+a = "Hello world"
+print(a)
+
+b = 5
+c = 7
+print(b + c)
+print(-1 - (-5))
